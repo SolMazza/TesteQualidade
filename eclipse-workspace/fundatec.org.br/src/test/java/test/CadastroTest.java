a028d6b9ec08ef5990afe481c43c2b744c438103
Sol Mazza <145726469+SolMazza@users.noreply.github.com>
2025-02-18 21:57:32 -0300

    Add files via upload

---
+++ eclipse-workspace/fundatec.org.br/src/test/java/test/CadastroTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import pages.CadastroPages;
+
+public class CadastroTest {
+
+	static WebDriver driver;
+	static CadastroPages cadastroPages; 
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.setProperty("webDriver.chrome.driver", "C:\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://accounts.google.com/SignUp?hl=pt");
+		driver.manage().window().maximize();
+		cadastroPages = new CadastroPages(driver);
+	}
+
+	@Test
+	public void test() {
+		
+		cadastroPages.preencherFirstName();
+		cadastroPages.preencherLastName();
+		cadastroPages.clicarBotaoProximo();
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+
+
+}
